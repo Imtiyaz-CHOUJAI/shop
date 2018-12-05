bc1b23932ae2b725e5d52bbd0b6de3c8af816240
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-06 00:29:57 +0100

    Create likes migration

---
+++ database/migrations/2018_12_05_205325_create_likes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('shop_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('shop_id')
+                ->references('id')
+                ->on('shops');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}

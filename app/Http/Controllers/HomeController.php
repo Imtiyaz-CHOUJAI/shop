59a3637054a8b9648b5b2af327f799cb34297878
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-06 00:34:36 +0100

    Setup the home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Shop;
+use JavaScript;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $shops = Shop::all();
+
+        JavaScript::put([
+            'shops' => $shops
+        ]);
+
+        return view('home');
+    }
+}

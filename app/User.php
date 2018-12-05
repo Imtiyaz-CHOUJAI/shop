afd0f734a607367eed20af530274885a160bc30a
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-06 00:27:38 +0100

    Update User model to include relations

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
 
     /**
@@ -27,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
+    public function shops()
+    {
+        return $this->hasMany('App\Shop');
+    }
 }

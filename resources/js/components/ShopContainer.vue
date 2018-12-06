78720b870c07988357599c1832bb10e153aa9a5e
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-06 23:28:19 +0100

    Update shops functionnality

--- resources/js/components/ShopContainer.vue
+++ resources/js/components/ShopContainer.vue
@@ -1,26 +1,19 @@
 <template>
     <div>
         <ul class="cd-items cd-container">
-            <li class="cd-item" v-for="shop in shops" :key="shop.id">
-                <img src="img/item-3.jpg" alt="Item Preview">
-                <a href="#0" class="cd-trigger" @click="like(shop)">View {{ shop.name }}</a>
-            </li>
+            <shop v-for="shop in shops" :key="shop.id" :shop="shop"></shop>
         </ul>
     </div>
 </template>
 <script>
     export default {
-        mounted() {
-        },
         data() {
             return {
-                shops: shop.shops
+                shops: shop.shops,
             }
         },
-        methods: {
-            like(shop) {
-                console.log('like', shop)
-            }
+        components: {
+            Shop: require('./Shop.vue'),            
         }
     }
 </script>

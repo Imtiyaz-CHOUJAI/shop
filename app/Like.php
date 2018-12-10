c5b19e4507e15c37d1945b684c74f4764400b9f0
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-11 00:34:05 +0100

    Add fillables to the Like model

--- app/Like.php
+++ app/Like.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'shop_id', 'user_id'
+    ];
+
     public function shop()
     {
         return $this->belongsTo('App\Shop', 'shop_id', 'id');

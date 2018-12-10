80acdf900a322e661e31ff33bed4c9c651bfa63f
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-11 00:49:06 +0100

    Change display method and add routes to navigate to preffered shops

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,16 +1,35 @@
+<style>
+    .link {
+        padding-left: 15px;
+    }
+    .active {
+        color: #000;
+        text-decoration: underline;
+    }
+</style>
+
 <template>
     <div>
-        <shop-container></shop-container>
-        <!-- <shop-details></shop-details> -->
+        <div class="text-right">
+            <router-link to="/" class="link" :class="{'active': active('all')}">All shops</router-link>
+            <router-link to="/liked" class="link" :class="{'active': active('liked')}">Prefferd shops</router-link>
+        </div>
+
+        <router-view></router-view>
     </div>
 </template>
 <script>
     export default {
         mounted() {
-            console.log('App mounted')
         },
-        components: {   
-            ShopContainer: require('./components/ShopContainer.vue'),
-        }    
+        methods: {
+            active(pageName) {
+                if (this.$route.name === pageName) {
+                    return true
+                }
+
+                return false
+            }
+        }
     }
 </script>

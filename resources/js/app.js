c51af508e7d0299364a00959c8d533e929822463
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-11 00:48:10 +0100

    Change render method for main app

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import swal from 'sweetalert'
 import router from './router/index.js'
 import store from './store.js'
+let App = require('./app.vue')
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
 const app = new Vue({
-    el: '#app',
+    el: '#shop-app',
     router,
-    store
+    store,
+    render: h => h(App)
 });

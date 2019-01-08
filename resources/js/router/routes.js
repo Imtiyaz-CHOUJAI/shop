3fbf86e1d5146b5a143197efa35d49b49fda4319
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-01-08 23:45:59 +0100

    Format file

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,17 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
 let routes = [
-	{
-		path : '/',
-		component: require('../components/Shops.vue'),
-		name: 'all'
-	},
-	{
-		path: '/liked',
-		component: require('../components/Liked.vue'),
-		name: 'liked'
-	}
-]
+    {
+        path: "/",
+        component: require("../components/Shops.vue"),
+        name: "all"
+    },
+    {
+        path: "/liked",
+        component: require("../components/Liked.vue"),
+        name: "liked"
+    }
+];
 
 export default routes;

b933c51f8b7670ed5771c4040f2e05b2af72f261
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-01-08 23:52:01 +0100

    Display liked shops in preffered shops page

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,35 +1,34 @@
 <style>
-    .link {
-        padding-left: 15px;
-    }
-    .active {
-        color: #000;
-        text-decoration: underline;
-    }
+.link {
+  padding-left: 15px;
+}
+.active {
+  color: #000;
+  text-decoration: underline;
+}
 </style>
 
 <template>
-    <div>
-        <div class="text-right">
-            <router-link to="/" class="link" :class="{'active': active('all')}">All shops</router-link>
-            <router-link to="/liked" class="link" :class="{'active': active('liked')}">Prefferd shops</router-link>
-        </div>
-
-        <router-view></router-view>
+  <div>
+    <div class="text-right">
+      <router-link to="/" class="link" :class="{'active': active('all')}">All shops</router-link>
+      <router-link to="/liked" class="link" :class="{'active': active('liked')}">Prefferd shops</router-link>
     </div>
+
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-    export default {
-        mounted() {
-        },
-        methods: {
-            active(pageName) {
-                if (this.$route.name === pageName) {
-                    return true
-                }
+export default {
+  mounted() {},
+  methods: {
+    active(pageName) {
+      if (this.$route.name === pageName) {
+        return true;
+      }
 
-                return false
-            }
-        }
+      return false;
     }
+  }
+};
 </script>

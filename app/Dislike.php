c55f9a9de00919de56cc16003d81007b0eb3c8eb
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-01-08 23:46:42 +0100

    Add Dislike model

---
+++ app/Dislike.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dislike extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'shop_id', 'user_id'
+    ];
+
+    public function shop()
+    {
+        return $this->belongsTo('App\Shop', 'shop_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+}

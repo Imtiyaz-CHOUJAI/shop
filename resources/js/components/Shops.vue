9bc93e294c7fb329e4be52e22da3dfa690659bf0
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-01-08 23:50:07 +0100

    Format file

--- resources/js/components/Shops.vue
+++ resources/js/components/Shops.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>
-        <ul class="cd-items cd-container">
-            <shop v-for="shop in shops" :key="shop.id" :shop="shop"></shop>
-        </ul>
-    </div>
+  <div>
+    <ul class="cd-items cd-container">
+      <shop v-for="shop in shops" :key="shop.id" :shop="shop"></shop>
+    </ul>
+  </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                shops: shop.shops,
-            }
-        },
-        components: {
-            Shop: require('./Shop.vue'),            
-        }
-    }
+export default {
+  data() {
+    return {
+      shops: shop.shops
+    };
+  },
+  components: {
+    Shop: require("./Shop.vue")
+  }
+};
 </script>

292515e77a5a0fd7c5c6a4c66b86953a7404f2ea
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-01-08 23:52:28 +0100

    Display liked shops in preffered shops page

--- resources/js/components/Liked.vue
+++ resources/js/components/Liked.vue
@@ -1,10 +1,29 @@
 <template>
+  <div>Liked shops to display here
     <div>
-        Liked shops to display here
+      <ul class="cd-items cd-container">
+        <shop v-for="shop in likedShops" :key="shop.id" :shop="shop" :likesPage="true"></shop>
+      </ul>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        
+export default {
+  data() {
+    return {
+      shops: shop.shops
+    };
+  },
+  components: {
+    Shop: require("./Shop.vue")
+  },
+  computed: {
+    likedShops() {
+      return this.shops.filter(
+        item =>
+          item.likes.length !== 0 && item.likes[0].user_id === shop.user.id
+      );
     }
+  }
+};
 </script>
